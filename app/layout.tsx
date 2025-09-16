dda1d6f5e812a70ea64b809780a03f850e75b5c1
mrgentil <tshitshob@gmail.com.com>
2025-09-16 23:24:46 +0100

    fixed_layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
 import ToasterProvider from "@/components/toaster-provider";
+import React from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -12,10 +13,10 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "Pulsar360 - Marketing Digital Studio",
   description:
-    "Plateforme SaaS de marketing digital pour optimiser vos campagnes sur les réseaux sociaux",
+    "Plateforme SaaS de marketing digital pour optimiser vos campagnes sur les reseaux sociaux",
   keywords: [
     "marketing digital",
-    "réseaux sociaux",
+    "reseaux sociaux",
     "automation",
     "analytics",
     "content management",
@@ -41,49 +42,46 @@ export default function RootLayout({
         <link rel="stylesheet" href="/css/bootstrap.min.css" />
         <link rel="stylesheet" href="/css/aos.css" />
         <link rel="stylesheet" href="/css/style.css" />
-          <Script src="/assets/js/layout.js"></Script>
-              <link
-                  href="/assets/libs/jsvectormap/css/jsvectormap.min.css"
-                  rel="stylesheet"
-                  type="text/css"
-              />
-              <link
-                  href="/assets/libs/swiper/swiper-bundle.min.css"
-                  rel="stylesheet"
-                  type="text/css"
-              />
-              <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-              <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-              <link href="/assets/css/app.min.css" rel="stylesheet" type="text/css" />
-              <link href="/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
-
+          <link
+              href="/assets/libs/jsvectormap/css/jsvectormap.min.css"
+              rel="stylesheet"
+              type="text/css"
+          />
+          <link
+              href="/assets/libs/swiper/swiper-bundle.min.css"
+              rel="stylesheet"
+              type="text/css"
+          />
+          <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+          <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+          <link href="/assets/css/app.min.css" rel="stylesheet" type="text/css" />
+          <link href="/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
       </head>
       <body className="font-sans antialiased">
         <div className="page_wrapper">
-          <div id="preloader">
-
-          </div>
-          {/* Header du site géré dans app/(site)/layout.tsx pour éviter les doublons */}
+          <div id="preloader"></div>
+          {/* Header du site gere dans app/(site)/layout.tsx pour eviter les doublons */}
           {children}
           <ToasterProvider />
         </div>
         <Script src="/js/jquery.js" strategy="beforeInteractive" />
         <Script src="/js/owl.carousel.min.js" strategy="afterInteractive" />
         <Script src="/js/bootstrap.min.js" strategy="afterInteractive" />
-        <Script src="/js/aos.js" strategy="afterInteractive" />
-        <Script src="/js/main.js" strategy="afterInteractive" />
-        <Script src="/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></Script>
-        <Script src="/assets/libs/simplebar/simplebar.min.js"></Script>
-        <Script src="/assets/libs/node-waves/waves.min.js"></Script>
-        <Script src="/assets/libs/feather-icons/feather.min.js"></Script>
-        <Script src="/assets/js/pages/plugins/lord-icon-2.1.0.js"></Script>
-        <Script src="/assets/js/plugins.js"></Script>
-        <Script src="/assets/libs/apexcharts/apexcharts.min.js"></Script>
-        <Script src="/assets/libs/jsvectormap/js/jsvectormap.min.js"></Script>
-        <Script src="/assets/libs/jsvectormap/maps/world-merc.js"></Script>
-        <Script src="/assets/libs/swiper/swiper-bundle.min.js"></Script>
-        <Script src="/assets/js/pages/dashboard-ecommerce.init.js"></Script>
-        <Script src="/assets/js/app.js"></Script>
+        <Script src="/js/aos.js" strategy="lazyOnload" />
+        <Script src="/js/main.js" strategy="lazyOnload" />
+        <Script src="/assets/js/layout.js" strategy="beforeInteractive" />
+        <Script src="/assets/libs/bootstrap/js/bootstrap.bundle.min.js" strategy="afterInteractive" />
+        <Script src="/assets/libs/simplebar/simplebar.min.js" strategy="afterInteractive" />
+        <Script src="/assets/libs/node-waves/waves.min.js" strategy="afterInteractive" />
+        <Script src="/assets/libs/feather-icons/feather.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/pages/plugins/lord-icon-2.1.0.js" strategy="lazyOnload" />
+        <Script src="/assets/js/plugins.js" strategy="afterInteractive" />
+        <Script src="/assets/libs/apexcharts/apexcharts.min.js" strategy="lazyOnload" />
+        <Script src="/assets/libs/jsvectormap/js/jsvectormap.min.js" strategy="lazyOnload" />
+        <Script src="/assets/libs/jsvectormap/maps/world-merc.js" strategy="lazyOnload" />
+        <Script src="/assets/libs/swiper/swiper-bundle.min.js" strategy="afterInteractive" />
+        <Script src="/assets/js/pages/dashboard-ecommerce.init.js" strategy="lazyOnload" />
+        <Script src="/assets/js/app.js" strategy="afterInteractive" />
       </body>
     </html>
   );
